b32f942ed2c3b0cce2edc9049d974eeb5548351b
Mujjkir-074 <ansarimujjkir786@gmail.com>
2024-02-28 20:01:08 +0530

    ui and wallet changes

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -10,3 +10,24 @@ server.listen(port, () => {
     SocketService_1.default.connect();
     console.log('Socket Connected');
 });
+// import { Server } from './server';
+// const server: any = require('http').Server(new Server().app);
+// import socketObj from './services/SocketService';
+// import cluster from 'cluster';
+// import * as os from 'os'
+// const totalCpu = os.cpus().length;
+// console.log(totalCpu)
+// console.log("cluster",cluster)
+// if(cluster.isPrimary){
+//     for(let i = 0; i < totalCpu; i++){
+//         cluster.fork();
+//     }
+// }else{  
+// let port = process.env.PORT || 8002;
+// server.listen(port, () => {
+// 	console.log(`server is listening at port ${port}`);
+// 	socketObj.init(server);
+// 	socketObj.connect();
+// 	console.log('Socket Connected');
+// });
+// }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,3 +9,26 @@ server.listen(port, () => {
 	console.log('Socket Connected');
 });
 
+// import { Server } from './server';
+// const server: any = require('http').Server(new Server().app);
+// import socketObj from './services/SocketService';
+// import cluster from 'cluster';
+// import * as os from 'os'
+
+// const totalCpu = os.cpus().length;
+// console.log(totalCpu)
+// console.log("cluster",cluster)
+// if(cluster.isPrimary){
+//     for(let i = 0; i < totalCpu; i++){
+//         cluster.fork();
+//     }
+// }else{  
+// let port = process.env.PORT || 8002;
+// server.listen(port, () => {
+// 	console.log(`server is listening at port ${port}`);
+// 	socketObj.init(server);
+// 	socketObj.connect();
+// 	console.log('Socket Connected');
+// });
+// }
+
