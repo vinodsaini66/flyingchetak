e3cd9b86b094b5d1bc376efd8ef2739f62bfa551
Mujjkir-074 <ansarimujjkir786@gmail.com>
2024-04-17 14:36:40 +0530

    game section all game

--- backend/dist/constants/project.enum.js
+++ backend/dist/constants/project.enum.js
@@ -5,6 +5,6 @@ exports.CONASTANT = {
     DB_URL: "mongodb+srv://yashpal:Yashpal1990@coachingappcluster.rkruzgs.mongodb.net/flyingChetak?retryWrites=true&w=majority",
     JWT_SECRET: "wingsFly",
     JWT_TIMEOUT_DURATION: "6400000",
-    REDIRECTION_URL: "http://35.202.132.15/api/app/wallet/add/balance-via-third-party",
-    BASE_URL: "http://35.202.132.15"
+    REDIRECTION_URL: "http://35.222.165.21/api/app/wallet/add/balance-via-third-party",
+    BASE_URL: "http://35.222.165.21"
 };
