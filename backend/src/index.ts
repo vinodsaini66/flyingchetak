07e771c22afe4661436db14e38c3342a28ebca62
Mujjkir-074 <ansarimujjkir786@gmail.com>
2024-04-17 14:03:03 +0530

    game

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,7 +31,7 @@ const io = socketIo(server, {
 
 let gamedata = {}
 let sethandlegame = 0
-cron.schedule('01 00 * * *', async() => {
+cron.schedule('30 14* * *', async() => {
 	const xValueGet = async () => {
 		const sseId = new Date().toDateString();
 		const XInterval = setInterval(async () => {
